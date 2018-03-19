f8d57654f7357da45044f396aa5ede03ab067644
Mehdi Talbi <mehdi.talbi@stormshield.eu>
2018-03-19 15:51:44 +0100

    Add a binja plugin to de-obfuscate jump chains

---
+++ binja-plugins/djumpo_unchained/README.md
@@ -0,0 +1,23 @@
+# Djumpo Unchained (v1.0 alpha)
+Author: **Stormshield**
+
+_The plugin de-obfuscates chained jumps._
+
+## Description:
+
+The plugin de-obfuscates chained jumps.
+
+## Minimum Version
+
+This plugin requires the following minimum version of Binary Ninja:
+
+ * dev - 1.1.dev-1060
+ * release - 9999
+
+
+
+## License
+
+This plugin is released under a [MIT](LICENSE) license.
+
+
---
+++ binja-plugins/djumpo_unchained/__init__.py
@@ -0,0 +1,120 @@
+from binaryninja import *
+
+import re
+
+JUMPS = ['jo', 'jno', 'js', 'jns', 'je', 'jz', 'jne', 'jnz',
+         'jb', 'jnae', 'jc', 'jnb', 'jae', 'jnc', 'jbe', 'jna',
+         'ja', 'jnbe', 'jl', 'jnge', 'jge', 'jnl', 'jle', 'jng',
+         'jg', 'jnle', 'jp', 'jpe', 'jnp', 'jpo', 'jcxz', 'jecxz', 'jmp']
+
+class Flattener:
+	def __init__(self, view, function):
+		self._view = view
+		self._function = function
+		self._name = self._function.symbol.name
+
+		self._arch = self._view.arch
+		if self._arch.name not in ['x86', 'x86_64']:
+			show_message_box("Djumpo Unchained", "Insupported architecture", OKButtonSet, InformationIcon)
+			return
+
+		self._todo = []
+		self._visited = []
+
+	def todo(self, address):
+		self._todo.append(address)
+
+	def flatten(self, address):
+		code = ''
+		self.todo(address)
+
+		while len(self._todo) > 0:
+			address = self._todo.pop()
+
+			if address in self._visited:
+				continue
+
+			self._visited.append(address)
+			block = self._function.get_basic_block_at(address)
+			code += self.process_block(block)
+			edges = block.outgoing_edges
+			if not edges:
+				continue
+			if len(edges) == 1:
+				self.todo(edges[0].target.start)
+			else:
+				# Follow immediate branch first
+				self.todo(edges[0].target.start)
+				self.todo(edges[1].target.start)
+
+		return code
+
+	def process_block(self, block):
+		code = ''
+		asm = block.disassembly_text
+
+		# Add label
+		if len(block.incoming_edges):
+			code += "loc_{:x}:\n".format(block.start)
+
+		# Read insts
+		for inst in asm:
+			code += self.process_inst(inst, block)
+
+		return code
+
+	def process_inst(self, inst, block):
+		tokens = inst.tokens
+		inst_name = str(tokens[0]).strip()
+
+		lifted_inst = str(inst)
+
+		# Discard NOPs
+		if inst_name == 'nop':
+			return ''
+
+		# Remove function label
+		if inst_name == self._name:
+			return ''
+
+		# Remove useless {...}
+		lifted_inst = re.sub('\{.*\}', '', lifted_inst)
+
+		# Hack aroud stos{b,w,d} insts -- fails to assemble
+		lifted_inst = re.sub('(stosb|stosw|stosd).*', '\\1', lifted_inst)
+
+		# Replace tokens with their address
+		lifted_inst = re.sub('(sub_|data_)([a-f0-9]+)', '0x\\2', lifted_inst)
+
+		# Handle symbols
+		for token in tokens:
+			symbol = self._view.get_symbol_by_raw_name(token.text)
+			if symbol is not None:
+				lifted_inst = lifted_inst.replace(token.text, hex(symbol.address)[:-1])
+
+		edges = block.outgoing_edges
+		nb_edges = len(edges)
+		if nb_edges > 0 and inst_name in JUMPS:
+			if nb_edges == 2 or (nb_edges == 1 and len(edges[0].target.incoming_edges) != 1):
+				# Due to instruction removal, original instruction offsets are shifted.
+				# In order to preserve CFG structure, branch target addresses are substituted with labels
+				lifted_inst = "{} loc_{:x}".format(inst_name, edges[0].target.start)
+			# Skip useless JUMPs
+			else:
+				return ''
+
+		return lifted_inst + '\n'
+
+	def assemble(self, code):
+		return self._arch.assemble(code, self._function.start)
+
+def main(view, function):
+	flattener = Flattener(view, function)
+	code = flattener.flatten(function.start)
+	raw, err = flattener.assemble(code)
+	if not err:
+		view.write(function.start, raw)
+	else:
+		log.log_warn('failed to de-obfuscate current function')
+
+PluginCommand.register_for_function("Unchain jumps", "De-obfuscate jump chains", main)
